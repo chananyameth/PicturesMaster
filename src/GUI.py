ee8c2d66b50af402f0fcf1012c690e68a5e31b0c
Chananya meth <chananyameth+gitlab@gmail.com>
2025-01-19 01:49:51 +0200

    Remove some unused code, better names

--- src/GUI.py
+++ src/GUI.py
@@ -1,12 +1,16 @@
-import sys
+import logging
 import subprocess
-import random
-import os
-import time
-from PySide6.QtWidgets import QApplication, QFileDialog, QVBoxLayout, QWidget, QLineEdit, QPushButton, QLabel, QGridLayout, QSizePolicy
+import sys
+
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QIcon
-from PySide6.QtSql import QSqlDatabase, QSqlQuery
+from PySide6.QtWidgets import QApplication, QFileDialog, QWidget, QLineEdit, QPushButton, QLabel, QGridLayout, \
+    QSizePolicy
+
+from src.copy_from_phone import copy_from_phone
+from src.status_history import StatusHistory
+
+logging.basicConfig(level=logging.NOTSET)
 
 
 class MainApp(QWidget):
@@ -25,11 +29,13 @@ class MainApp(QWidget):
 
         labels['Source'] = QLabel('Source')
         labels['Destination'] = QLabel('Destination')
+        labels['Command'] = QLabel('ADB Command')
         labels['Source'].setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
         labels['Destination'].setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
 
         self.lineEdits['Source'] = QLineEdit()
         self.lineEdits['Destination'] = QLineEdit()
+        self.lineEdits['Command'] = QLineEdit()
 
         layout.addWidget(labels['Source'],            0, 0, 1, 1)
         layout.addWidget(self.lineEdits['Source'],    0, 1, 1, 3)
@@ -37,15 +43,17 @@ class MainApp(QWidget):
         layout.addWidget(labels['Destination'],            1, 0, 1, 1)
         layout.addWidget(self.lineEdits['Destination'],    1, 1, 1, 3)
 
-        button_login = QPushButton('&Log In', clicked=self.check_credential)
-        layout.addWidget(button_login,                  2, 3, 1, 1)
+        layout.addWidget(labels['Command'],            2, 0, 1, 1)
+        layout.addWidget(self.lineEdits['Command'],    2, 1, 1, 3)
+        button_run_command = QPushButton('&Run Command', clicked=self.run_command)
+        layout.addWidget(button_run_command,                  2, 4, 1, 1)
 
         self.status = QLabel('put here the status')
         self.status.setStyleSheet('font-size: 15px; color: red; background: yellow')
+        StatusHistory.add_on_update_handler_full_history(self.status.setText)
         layout.addWidget(self.status, 3, 0, 1, 4)
 
-
-        button1 = QPushButton("Copy from phone")
+        button1 = QPushButton("Copy from phone", clicked=copy_from_phone)
         button2 = QPushButton("Standardize files' names")
         button3 = QPushButton("Flat folder to tree")
         button4 = QPushButton("Click me! get directory", clicked=self.get_directory_path)
@@ -54,8 +62,9 @@ class MainApp(QWidget):
         layout.addWidget(button2, 4, 1)
         layout.addWidget(button3, 4, 2)
         layout.addWidget(button4, 5, 0, 1, 1)
-        layout.addWidget(QPushButton("Choose", clicked=lambda: self.get_directory_path("Source")), 0, 4, 1, 1)
-        layout.addWidget(QPushButton("Choose", clicked=lambda: self.get_directory_path("Destination")), 1, 4, 1, 1)
+        layout.addWidget(QPushButton("Choose source dir", clicked=lambda: self.get_directory_path("Source")), 0, 4, 1, 1)
+        layout.addWidget(QPushButton("Choose destination dir", clicked=lambda: self.get_directory_path("Destination")), 1, 4, 1, 1)
+
 
     def get_directory_path(self, var_name='dir_path'):
         dlg = QFileDialog(self, Qt.WindowType.Dialog)
@@ -73,22 +82,13 @@ class MainApp(QWidget):
         else:
             self.status.setText("Can't get directory from dialog")
 
-    def check_credential(self):
-        username = self.lineEdits['Source'].text()
-        password = self.lineEdits['Destination'].text()
-        print(username, password)
-
-        # commands = ["ls /storage/emulated/0/DCIM/Camera\n", "pwd"]
-        commands = [username + '\n']
-        output = subprocess.check_output([r"C:\Users\Chananya\Downloads\adb\adb.exe", "shell", *commands])
+    def run_command(self):
+        # commands = ["ls /storage/emulated/0/DCIM/Camera | wc -l\n", "pwd"]
+        command = self.lineEdits['Command'].text()
+        commands = [command + '\n']
+        output = subprocess.check_output([r"C:\Chananya\Software\adb\adb.exe", "shell", *commands])
         for line in output.split(b'\r\n'):
             print(line.decode("utf-8"))
-
-    #     self.button.clicked.connect(self.magic)
-    #
-    # @QtCore.Slot()
-    # def magic(self):
-    #     self.text.setText(random.choice(self.hello))
 
     @property
     def source_path(self):
